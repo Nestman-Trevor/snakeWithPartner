347d2fd847bbb02f65994b05a69d9e82ae217700
tnestman <trevornestman@192.168.1.142>
2014-01-22 19:01:51 -0700

    Initial commit

---
+++ src/snakewithpartner/SnakeWithPartner.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package snakewithpartner;
+
+/**
+ *
+ * @author trevornestman
+ */
+public class SnakeWithPartner {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
