9d630411f5f88535ba74b6208611aedcc90bf1d9
tnestman <trevornestman@192.168.1.142>
2014-01-23 18:38:53 -0700

    Initial commit

--- src/snakewithpartner/SnakeWithPartner.java
+++ src/snakewithpartner/SnakeWithPartner.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package snakewithpartner;
+
+import java.util.Scanner;
 
 /**
  *
@@ -12,11 +13,28 @@ package snakewithpartner;
  */
 public class SnakeWithPartner {
 
+    String name;
+    String instructions = "Welcome to Snake!\nVersion: 1.0\n"
+            + "The goal is to eat the food and not yourself./n"
+            + "Also, avoid trying to leave the area.";
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        SnakeWithPartner myGame = new SnakeWithPartner();
+        myGame.getName();
+        myGame.displayHelp();
     }
-    
+
+    public void getName() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        this.name = input.next();
+    }
+
+    public void displayHelp() {
+        System.out.println("/nWelcome " + this.name + "\n");
+        System.out.println(this.instructions);
+    }
 }
