2bf0ea0ceb322099669fca1fa7b2c6d42b0c8014
Josh <Josh@Vader>
2014-01-25 18:42:38 -0700

    Added instance variables for each menu option of the Game Over menu, made the function to display them.

--- src/snakewithpartner/GameOverMenu.java
+++ src/snakewithpartner/GameOverMenu.java
@@ -8,8 +8,18 @@ package snakewithpartner;
 
 /**
  *
- * @author trevornestman
+ * @author jmarchello
  */
 public class GameOverMenu {
+    //Menu items
+    String option1 = "Play Again";
+    String option2 = "High Scores";
+    String option3 = "Exit";
     
+    //Display Menu Items
+    public void displayMenu(){
+        System.out.println("1. " + option1 + "\n");
+        System.out.println("2. " + option2 + "\n");
+        System.out.println("3. " + option3 + "\n");
+    }
 }
