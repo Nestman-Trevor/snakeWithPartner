1ca88193379b1aeb53672a4657246ad0fd86d12c
tnestman <trevornestman@192.168.1.142>
2014-01-30 00:14:25 -0700

    Created

--- src/snakewithpartner/HighScore.java
+++ src/snakewithpartner/HighScore.java
@@ -5,6 +5,8 @@
  */
 package snakewithpartner;
 
+import java.util.Scanner;
+
 /**
  *
  * @author trevornestman
@@ -14,46 +16,79 @@ public class HighScore {
     //High score title
     String highScoreTitle = "High Scores";
     //Declares the array of High Scores
-    int easyHighScoreList[] = new int[10];
+    int[] easyHighScoreList = {1000, 900, 800, 700, 600, 500, 400, 300, 200, 100};
     int mediumHighScoreList[] = new int[10];
     int hardHighScoreList[] = new int[10];
+    //Creates an array that is used to copy over high scores in temporary storage
     int tempArray[] = new int[10];
+    
+    double userScore;
+    String userInput;
 
     //Display everything in High Score
-    public void displayHighScoreInfo() {
+    public void displayHighScoreInfo(int[] highScoreList, String highScoreTitle) {
         System.out.println(highScoreTitle + "\n");
-        for (int i = 0; i <= 9; i++) {
-            System.out.println("\n" + i + ". " + easyHighScoreList[i]);
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i + ". " + highScoreList[i - 1]);
         }
     }
-
-    public void easyComputeHighScore(int score) {
-        //sets temperary array
-        for (int r = 0; r < 10; r++) {
-            tempArray[r] = easyHighScoreList[r];
-        }
-        //creates new array with scores moved and lowest score dropped
-        for (int i = 0; i < 10; i++) {
-            if (score >= easyHighScoreList[i]) {
-                tempArray[i] = score;
-                if (i != 9) {
-                    for (int x = i + 1; x < 10; x++) {
-                        tempArray[x] = easyHighScoreList[x - 1];
-                    }
-                }
-                break;
-            } else {
-                tempArray[i] = easyHighScoreList[i];
+    
+    private boolean isValidScore(int score) {
+        boolean isValid = (score >= 0);
+        return isValid;
+    }
+    
+    public void easyComputeHighScore(double userScore) {
+        int score = (int) userScore;
+        if (isValidScore(score)) {
+            if (score < easyHighScoreList[9]) {
+                System.out.println("No high score");
+                return;
             }
-        }
-        for (int r = 0; r < 10; r++) {
-            easyHighScoreList[r] = tempArray[r];
+            //sets temperary array
+            for (int r = 0; r < 10; r++) {
+                tempArray[r] = easyHighScoreList[r];
+            }
+            //creates new array with scores moved and lowest score dropped
+            for (int i = 0; i < 10; i++) {
+                if (score >= easyHighScoreList[i]) {
+                    tempArray[i] = score;
+                    if (i != 9) {
+                        for (int x = i + 1; x < 10; x++) {
+                            tempArray[x] = easyHighScoreList[x - 1];
+                        }
+                    }
+                    break;
+                } else {
+                    tempArray[i] = easyHighScoreList[i];
+                }
+            }
+            for (int r = 0; r < 10; r++) {
+                easyHighScoreList[r] = tempArray[r];
+            }
+            displayHighScoreInfo(easyHighScoreList, "Easy High Score List");
+        } else {
+            System.out.println("NOT A \'VALID\' SCORE");
         }
     }
-
+    
     public void resetHighScoreList() {
         for (int f = 0; f <= 9; f++) {
             easyHighScoreList[f] = 0;
         }
+    }
+
+    public void testHighScore() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your score: ");
+        userInput = input.next();
+        userScore = userInput.length();
+        char [] classy = userInput.toCharArray();
+        for (int i = 0; i < classy.length; i++) {
+            if (classy[i] == '0') {
+                
+            }
+        }
+        this.easyComputeHighScore(userScore);
     }
 }
--- src/snakewithpartner/SnakeWithPartner.java
+++ src/snakewithpartner/SnakeWithPartner.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
  * @author trevornestman
  */
 public class SnakeWithPartner {
-
+    double finalScore;
     String name;
     String instructions = "Welcome to Snake!\nVersion: 1.0\n"
             + "The goal is to eat the food and not yourself.\n"
@@ -22,46 +22,8 @@ public class SnakeWithPartner {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        SnakeWithPartner myGame = new SnakeWithPartner();
-        myGame.getName();
-        myGame.displayHelp();
-        
-        Board gameBoard = new Board();
-        gameBoard.displayBoardSize();
-        
-        Food snakeFood = new Food();
-        snakeFood.displayScoreValue();
-        
-        GameOverMenu gameOver = new GameOverMenu();
-        gameOver.displayMenu();
-        
-        Help gameHelp = new Help();
-        gameHelp.displayHelp();
-        
-        HighScore gameHighScore = new HighScore();
-        gameHighScore.displayHighScoreInfo();
-        
-        MainMenu gameMainMenu = new MainMenu();
-        gameMainMenu.displayTitles();
-        
-        PlayMenu gamePlayMenu = new PlayMenu();
-        gamePlayMenu.displayPlayMenuStrings();
-        
-        Player player1 = new Player();
-        player1.displayName();
-        
-        Score currentScore = new Score();
-        currentScore.displayScore();
-        
-        Settings settings = new Settings();
-        settings.displaySettings();
-        
-        Snake snake = new Snake();
-        snake.displaylength();
-        
-        Time time = new Time();
-        time.showTimeInSeconds();
-        
+        HighScore myScore = new HighScore();
+        myScore.testHighScore();
     }
 
     public void getName() {
