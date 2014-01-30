e87d8611b26eb0466dc6b04b68452a594034b987
tnestman <trevornestman@192.168.1.142>
2014-01-29 22:21:18 -0700

    Created

--- src/snakewithpartner/HighScore.java
+++ src/snakewithpartner/HighScore.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package snakewithpartner;
 
 /**
@@ -11,27 +10,50 @@ package snakewithpartner;
  * @author trevornestman
  */
 public class HighScore {
+
     //High score title
     String highScoreTitle = "High Scores";
     //Declares the array of High Scores
-    int highScoreList[] = new int[10];
-    
+    int easyHighScoreList[] = new int[10];
+    int mediumHighScoreList[] = new int[10];
+    int hardHighScoreList[] = new int[10];
+    int tempArray[] = new int[10];
+
     //Display everything in High Score
-    public void displayHighScoreInfo(){
+    public void displayHighScoreInfo() {
         System.out.println(highScoreTitle + "\n");
-        for(int i = 0;i <= 9;i++){
-            System.out.println("\n" + i + ". " + highScoreList[i]);
+        for (int i = 0; i <= 9; i++) {
+            System.out.println("\n" + i + ". " + easyHighScoreList[i]);
         }
     }
-    public void addToHighScore(int score){
-        if (score > highScoreList[9]){
-            
+
+    public void easyComputeHighScore(int score) {
+        //sets temperary array
+        for (int r = 0; r < 10; r++) {
+            tempArray[r] = easyHighScoreList[r];
         }
-        else{}
+        //creates new array with scores moved and lowest score dropped
+        for (int i = 0; i < 10; i++) {
+            if (score >= easyHighScoreList[i]) {
+                tempArray[i] = score;
+                if (i != 9) {
+                    for (int x = i + 1; x < 10; x++) {
+                        tempArray[x] = easyHighScoreList[x - 1];
+                    }
+                }
+                break;
+            } else {
+                tempArray[i] = easyHighScoreList[i];
+            }
+        }
+        for (int r = 0; r < 10; r++) {
+            easyHighScoreList[r] = tempArray[r];
+        }
     }
-    public void resetHighScoreList(){
-        for(int f = 0;f <= 9;f++){
-            highScoreList[f] = 0;
+
+    public void resetHighScoreList() {
+        for (int f = 0; f <= 9; f++) {
+            easyHighScoreList[f] = 0;
         }
     }
 }
