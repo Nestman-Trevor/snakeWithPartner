d3b748ee13b86a8c3f66129df63e66fe8ef38c26
tnestman <trevornestman@192.168.0.24>
2014-04-08 16:48:19 -0600

    TailSection is now a java bean and good to go with the right variables

--- src/snakewithpartner/TailSection.java
+++ src/snakewithpartner/TailSection.java
@@ -3,15 +3,42 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package snakewithpartner;
 
 import java.awt.Image;
+import java.io.Serializable;
 
 /**
  *
  * @author trevornestman
  */
-public class TailSection {
-        private Image ball;
+public class TailSection implements Serializable {
+
+    private Image ball;
+    private int xCordinate, yCordinate;
+
+    public TailSection() {
+    }
+
+    public int getxCordinate() {
+        return xCordinate;
+    }
+
+    public void setxCordinate(int xCordinate) {
+        this.xCordinate = xCordinate;
+    }
+
+    public int getyCordinate() {
+        return yCordinate;
+    }
+
+    public void setyCordinate(int yCordinate) {
+        this.yCordinate = yCordinate;
+    }
+
+    //Changes the x and y cordinates to the new location (movement of the tail)
+    public void moveSection(int xCordinate, int yCordinate) {
+        this.xCordinate = xCordinate;
+        this.yCordinate = yCordinate;
+    }
 }
